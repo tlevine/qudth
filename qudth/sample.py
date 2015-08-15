07d81e31fdd96db4e397a4b09d4b157664c308a3
Thomas Levine <_@thomaslevine.com>
2015-08-15 13:18:40 -0400

    census

--- qudth/sample.py
+++ qudth/sample.py
@@ -1,11 +1,24 @@
 import random
 
+def census(fp):
+    lines = set()
+    line = None
+
+    while line != '':
+        line_start = fp.tell()
+        line = fp.readline()
+        line_end = fp.tell()
+        lines.add((line_start, line_end))
+
+    return lines
+
+
 def srs(fp, n):
     '''
     If data are appended to the file during the function call,
     the appended data are ignored for the sampling.
     '''
-    file_start = 0
+    file_start = fp.tell()
     file_end = fp.seek(0, 2)
     lines = set()
     while len(lines) < n:
@@ -15,4 +28,7 @@ def srs(fp, n):
         fp.readline()
         line_end = fp.tell()
         lines.add((line_start, line_end))
+        if line == '':
+            break
+    fp.seek(file_start)
     return lines
