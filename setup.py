ba33b72f4d5c57f49bcc6331c266d7fb7fc121bd
Thomas Levine <_@thomaslevine.com>
2015-08-15 15:23:12 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='qudth',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Estimate the distributions of line lengths of files.',
+      url='http://dada.pink/qudth/',
+      packages=['qudth'],
+      install_requires = ['sparkprob>=0.1'],
+      version='0.0.1',
+      license='LGPL',
+      entry_points = {'console_scripts': ['qudth = qudth.cli:cli']},
+)
